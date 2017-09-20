45b3b92969b359b478ecccdc356e97d9a1db785f
amithamurali <murali.a@husky.neu.edu>
2017-09-20 18:23:49 -0400

    travis testing

--- csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/auth/WebSecurityConfig.java
+++ csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/auth/WebSecurityConfig.java
@@ -1,3 +1,9 @@
+/**
+ * Amitha_Murali, 001643826, murali.a@husky.neu.edu
+ * Jyoti Sharma, 001643410, sharma.j@husky.neu.edu
+ * Surabhi Patil, 001251860, patil.sur@husky.neu.edu
+ **/
+
 package com.csye6225.demo.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/controllers/HomeController.java
+++ csye6225-spring-boot-starter-webapp-master/src/main/java/com/csye6225/demo/controllers/HomeController.java
@@ -1,3 +1,9 @@
+/**
+ * Amitha_Murali, 001643826, murali.a@husky.neu.edu
+ * Jyoti Sharma, 001643410, sharma.j@husky.neu.edu
+ * Surabhi Patil, 001251860, patil.sur@husky.neu.edu
+ **/
+
 package com.csye6225.demo.controllers;
 
 
