3baf271406cb375c636ce4721e6b2484dcf6482a
amitha murali <murali.a@husky.neu.edu>
2017-09-28 15:44:58 -0400

    Submission assgn3 v2

--- src/main/java/com/csye6225/demo/controllers/HomeController.java
+++ src/main/java/com/csye6225/demo/controllers/HomeController.java
@@ -9,18 +9,11 @@ package com.csye6225.demo.controllers;
 import com.csye6225.demo.Helper;
 import com.csye6225.demo.UserRepository;
 import com.google.gson.JsonObject;
-import org.apache.catalina.connector.Response;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Test;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
