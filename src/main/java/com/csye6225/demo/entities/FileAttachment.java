bb2483b4a73f6548b4b49ecc625262c5c804fe9c
amitha murali <murali.a@husky.neu.edu>
2017-10-11 17:27:35 -0400

    push changes

---
+++ src/main/java/com/csye6225/demo/entities/FileAttachment.java
@@ -0,0 +1,57 @@
+package com.csye6225.demo.entities;
+
+import javax.persistence.*;
+
+/**
+ * Amitha_Murali, 001643826, murali.a@husky.neu.edu
+ * Jyoti Sharma, 001643410, sharma.j@husky.neu.edu
+ * Surabhi Patil, 001251860, patil.sur@husky.neu.edu
+ **/
+
+@Entity // This tells Hibernate to make a table out of this class
+public class FileAttachment {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String path;
+
+    private long taskId;
+
+    private int userId;
+
+    public long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(long taskId) {
+        this.taskId = taskId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+}
