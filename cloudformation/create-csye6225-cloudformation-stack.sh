107bae82dfc5984d11b19c8d57d4e7657d44b7c4
Surabhi Patil <patil.sur@husky.neu.edu>
2017-10-11 19:45:01 -0400

    save

--- cloudformation/create-csye6225-cloudformation-stack.sh
+++
@@ -1,5 +0,0 @@
-# Create CloudFormation Stack
-echo "Creating CloudFormation Stack"
-aws cloudformation create-stack --stack-name $1 --template-body file://./createStack.json
-
-aws cloudformation validate-template --template-body file://./createStack.json
