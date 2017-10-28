1d926b9be4c1c5a819253cc4682eafbb2162d314
amitha murali <murali.a@husky.neu.edu>
2017-10-28 12:02:55 -0400

    push changes

--- infrastructure/aws/cloudformation/create-csye6225-cloudformation-stack.sh
+++ infrastructure/aws/cloudformation/create-csye6225-cloudformation-stack.sh
@@ -6,31 +6,6 @@
 # Create CloudFormation Stack
 echo "Creating CloudFormation Stack"
 
-echo "Please enter a stack name"
-read stackName
-echo "Please enter vpcId"
-read vpcId
-echo "Please enter key name"
-read keyName
-echo "Please enter imageId"
-read imageId
-echo "Please enter snapshot Id"
-read snapshotId
-echo "Please enter the instance type"
-read InstType
-echo "Please enter the volume size"
-read volumeSize
-echo "Please enter the volume type"
-read volumeType
-echo "Please enter the hosted Zone Id"
-read hostedZoneId
-echo "Please enter the dns name"
-read dnsName
-echo "Please enter the RecordSet Type"
-read recordSetType
-echo "Please enter the RecordSeT Time to Live"
-read recordSetTTL
+aws cloudformation create-stack --stack-name CodeDeployDemoStack --template-url http://s3.amazonaws.com/aws-codedeploy-us-east-1/templates/latest/CodeDeploy_SampleCF_Template.json --parameters ParameterKey=InstanceCount,ParameterValue=1 ParameterKey=InstanceType,ParameterValue=t1.micro --ParameterKey=KeyPairName,ParameterValue=ec2Key ParameterKey=OperatingSystem,ParameterValue=Linux -ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=TagKey,ParameterValue=Name --ParameterKey=TagValue,ParameterValue=CodeDeployDemo --capabilities CAPABILITY_IAM
 
-aws cloudformation create-stack --stack-name $stackName --template-body file://./createStack.json --parameters ParameterKey=ImgId,ParameterValue=$imageId ParameterKey=VpcId,ParameterValue=$vpcId ParameterKey=KeyName,ParameterValue=$keyName ParameterKey=SnapshotId,ParameterValue=$snapshotId ParameterKey=InstType,ParameterValue=$InstType ParameterKey=volumeSize,ParameterValue=$volumeSize ParameterKey=volumeType,ParameterValue=$volumeType ParameterKey=hostedZoneId,ParameterValue=$hostedZoneId ParameterKey=dnsName,ParameterValue=$dnsName ParameterKey=recordSetType,ParameterValue=$recordSetType ParameterKey=recordSetTTL,ParameterValue=$recordSetTTL
-
-aws cloudformation validate-template --template-body file://./createStack.json
+aws cloudformation update-stack --stack-name CodeDeployDemoStack --template-body file://./updateStack1.json --capabilities CAPABILITY_IAM
