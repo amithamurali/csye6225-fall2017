26c477f4855996b24f57ae38c4826766ba136123
Surabhi Patil <patil.sur@husky.neu.edu>
2017-11-02 15:07:09 -0400

    added s3Client dependencies

--- src/main/java/com/csye6225/demo/controllers/FileAttachmentController.java
+++ src/main/java/com/csye6225/demo/controllers/FileAttachmentController.java
@@ -15,7 +15,6 @@ import com.google.gson.JsonObject;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,8 +28,6 @@ import java.io.File;
 import java.net.URL;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Base64;
-import java.util.List;
 
 @Controller    // This means that this class is a Controller
 
@@ -46,9 +43,20 @@ public class FileAttachmentController {
     @Autowired
     private TaskRepository taskRepository;
 
-    @RequestMapping(value = "/tasks/{id}/attachments", method = RequestMethod.GET, produces = "application/json")
-    public @ResponseBody
-    String getAttachedFilesForTask(HttpServletRequest request, HttpServletResponse response) {
+    @Autowired
+    private AmazonS3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucketName;
+
+
+
+
+    @Autowired
+    private S3ServicesImpl s3ServiceImpl;
+
+    @RequestMapping(value="/tasks/{id}/attachments",method= RequestMethod.GET,produces="application/json")
+    public @ResponseBody String getAttachedFilesForTask (HttpServletRequest request, HttpServletResponse response){
 
         JsonObject jsonObject = new JsonObject();
 
@@ -56,203 +64,220 @@ public class FileAttachmentController {
 
         String header = request.getHeader("Authorization");
         String taskId = request.getRequestURI().split("/")[2];
-        if (header != null) {
+        if(header != null) {
 
             int userID = helper.GetUserDetails(header);
 
-            if (userID > -1) {
+            if(userID > -1) {
 
-                if (taskId != "") {
+                if(taskId !="") {
                     Task task = taskRepository.findOne(taskId);
-                    if (task.getUserId() == userID) {
+                    if(task.getUserId() == userID) {
 
-                        ArrayList<String> fileList = helper.getFileList(taskId);
+                        ArrayList<String> fileList = helper.getFileList( taskId );
 
                         if (fileList == null) {
-                            jsonObject.addProperty("message", "No file exists.");
+                            jsonObject.addProperty( "message", "No file exists." );
                             return jsonObject.toString();
                         } else {
-                            for (String filePath : fileList) {
-                                jsonObject.addProperty("message", "File attached to the task " + filePath);
-                            }
+                            for(String filePath : fileList)
+                            {jsonObject.addProperty( "message", "File attached to the task " + filePath );}
                             return jsonObject.toString();
                         }
-                    } else {
-                        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                        jsonObject.addProperty("message", "You are not allowed to perform this activity.");
+                    }
+                    else {
+                        response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+                        jsonObject.addProperty( "message", "You are not allowed to perform this activity." );
                         return jsonObject.toString();
                     }
                 }
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.setStatus( HttpServletResponse.SC_BAD_REQUEST);
                 jsonObject.addProperty("message", "Invalid Task Id.");
                 return jsonObject.toString();
             }
-        } else {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+
+        else{
+            response.setStatus( HttpServletResponse.SC_UNAUTHORIZED);
             jsonObject.addProperty("message", "You are not authorized to perform this activity");
         }
         return jsonObject.toString();
 
     }
 
-    @RequestMapping(value = "/tasks/{id}/attachments", method = RequestMethod.POST, produces = "application/json")
-    public @ResponseBody
-    String attachFileToTask(HttpServletRequest request, HttpServletResponse response, @RequestParam("file") MultipartFile theFile) {
+    @RequestMapping(value="/tasks/{id}/attachments",method= RequestMethod.POST,produces="application/json")
+    public @ResponseBody String attachFileToTask (HttpServletRequest request, HttpServletResponse response, @RequestParam ("file")MultipartFile theFile){
 
         System.out.println("I am here");
-        JsonObject jsonObject = new JsonObject();
-        String header = request.getHeader("Authorization");
-        if (header != null && header.contains("Basic")) {
-            String[] credentialValues = decode(header);
-
-            User userExists = userService.findUserByEmail(credentialValues[0]);
-            if (userExists != null) {
-                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-                if (encoder.matches(credentialValues[1], userExists.getPassword()) || credentialValues[1].equals(userExists.getPassword())) {
-                    Task task = taskRepository.findOne(taskId);
-
-                    if (task.getUser().getId() == userExists.getId()) {
-                        if (task != null) {
-
-                            if (files.length != 0) {
-                                try {
-                                    String uploadsDir = "/uploads/";
-                                    String realPathtoUploads = request.getServletContext().getRealPath(uploadsDir);
-                                    if (!new File(realPathtoUploads).exists()) {
-                                        new File(realPathtoUploads).mkdir();
-                                    }
-
-
-                                    for (MultipartFile file : files) {
-                                        String orgName = file.getOriginalFilename();
-                                        String filePath = realPathtoUploads + orgName;
-
-                                        File dest = new File(filePath);
-                                        file.transferTo(dest);
-                                        String key = Instant.now().getEpochSecond() + "_" + dest.getName();
-                                        GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, key);
-                                        generatePresignedUrlRequest.setMethod(HttpMethod.GET);
-                                        generatePresignedUrlRequest.setExpiration(DateTime.now().plusDays(4).toDate());
-
-                                        URL signedUrl = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
-
-                                        FileUpload fileUpload = new FileUpload();
-                                        fileUpload.setFilePath(signedUrl.toString());
-                                        fileUpload.setTask(task);
-                                        fileUploadRepository.save(fileUpload);
-
-                                        s3ServiceImpl.uploadFile(key, dest);
-
-
-                                    }
-
-                                    if (files.length > 1) {
-                                        jsonObject.addProperty("message", "Files added successfully");
-                                    } else {
-                                        jsonObject.addProperty("message", "File added successfully");
-                                    }
-
-                                    response.setStatus(HttpServletResponse.SC_CREATED);
-                                } catch (Exception e) {
-                                    System.out.println(e);
-                                }
-
-                            } else System.out.println("******************File empty***************");
-
-
-                        } else {
-                            jsonObject.addProperty("message", "No task found with the given task id");
-                        }
-                    } else {
-                        jsonObject.addProperty("message", "unauthorized user accessing the task");
-                    }
-
-                } else
-                    jsonObject.addProperty("message", "Incorrect credentials");
-            } else {
-                jsonObject.addProperty("message", "Incorrect credentials");
-            }
-        } else {
-            jsonObject.addProperty("message", "You are not logged in !!");
-        }
-        return jsonObject.toString();
-
-
-    }
-
-    @RequestMapping(value = "/tasks/{id}/attachments/{idAttachments}", method = RequestMethod.DELETE, produces = "application/json")
-    public @ResponseBody
-    String deleteFile(HttpServletRequest request, HttpServletResponse response) {
 
         JsonObject jsonObject = new JsonObject();
-        String header = request.getHeader("Authorization");
-        if (header != null && header.contains("Basic")) {
-            String[] credentialValues = decode(header);
 
-            User userExists = userService.findUserByEmail(credentialValues[0]);
-            if (userExists != null) {
-                BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        //Write add file code here
 
-                if (encoder.matches(credentialValues[1], userExists.getPassword()) || credentialValues[1].equals(userExists.getPassword())) {
-                    Task task = taskRepository.findOne(taskId);
+        // location to store file uploaded
+        final String UPLOAD_DIRECTORY = "/home/surabhi/Documents/NSCC/Assignment 6 file upload";
 
-                    if (task.getUser().getId() == userExists.getId()) {
-                        if (task != null) {
+        // upload settings
+        final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
+        final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
+        final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
 
-                            List<FileUpload> attachments = task.getFiles();
+        String filePath = "";
+        // configures upload settings
+        DiskFileItemFactory factory = new DiskFileItemFactory();
+        // sets memory threshold - beyond which files are stored in disk
+        factory.setSizeThreshold(MEMORY_THRESHOLD);
+        // sets temporary location to store files
+        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
 
-                            FileUpload deleteFileUpload = new FileUpload();
+        ServletFileUpload upload = new ServletFileUpload(factory);
 
-                            for (FileUpload fu : attachments) {
+        // sets maximum size of upload file
+        upload.setFileSizeMax(MAX_FILE_SIZE);
 
-                                if (fu.getFileId().equals(idAttachment))
-                                    deleteFileUpload = fu;
+        // sets maximum size of request (include file + form data)
+        upload.setSizeMax(MAX_REQUEST_SIZE);
 
-                            }
+        // constructs the directory path to store upload file
+        // this path is relative to application's directory
+        //String uploadPath = getServletContext().getRealPath("")+ File.separator + UPLOAD_DIRECTORY;
 
-                            if (deleteFileUpload != null) {
-
-                                File deleteFile = new File(deleteFileUpload.getFilePath());
-                                deleteFile.delete();
-
-                                FileUpload deleteFileUploadDb = fileUploadRepository.findOne(deleteFileUpload.getFileId());
-
-                                fileUploadRepository.delete(deleteFileUploadDb);
-
-                                jsonObject.addProperty("message", "File deleted successfully");
-                                response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-
-
-                            } else {
-                                jsonObject.addProperty("message", "No attachment found with the given attachment id");
-                            }
-
-                        } else {
-                            jsonObject.addProperty("message", "No task found with the given task id");
-                        }
-                    } else {
-                        jsonObject.addProperty("message", "unauthorized user accessing the task");
-                    }
-
-
-                } else
-                    jsonObject.addProperty("message", "Incorrect credentials");
-            } else {
-                jsonObject.addProperty("message", "Incorrect credentials");
-            }
-        } else {
-            jsonObject.addProperty("message", "You are not logged in !!");
+        // creates the directory if it does not exist
+        File uploadDir = new File("/home/surabhi/Documents/NSCC/Assignment 6 file upload");
+        if (!uploadDir.exists()) {
+            uploadDir.mkdir();
         }
+
+        try {
+            // parses the request's content to extract file data
+
+            String key = Instant.now().getEpochSecond() + "_" + dest.getName();
+            GeneratePresignedUrlRequest generatePresignedUrlRequest = new GeneratePresignedUrlRequest(bucketName, key);
+            generatePresignedUrlRequest.setMethod(HttpMethod.GET);
+            generatePresignedUrlRequest.setExpiration(DateTime.now().plusDays(4).toDate());
+
+            URL signedUrl = s3Client.generatePresignedUrl(generatePresignedUrlRequest);
+            filePath = "/home/surabhi/Documents/NSCC/Assignment 6 file upload" + File.separator + theFile.getOriginalFilename();
+            File storeFile = new File(signedUrl.toString());
+
+
+                        // saves the file on disk
+                        //item.write(storeFile);
+            theFile.transferTo( storeFile );
+            s3ServiceImpl.uploadFile(key, dest);
+
+                        request.setAttribute("message",
+                                "Upload has been done successfully!");
+                   // }
+               // }
+           // }
+       } catch (Exception ex) {
+            request.setAttribute("message",
+                    "There was an error: " + ex.getMessage());
+        }
+
+        String header = request.getHeader("Authorization");
+        String taskId = request.getRequestURI().split("/")[2];
+        if(header != null) {
+
+            int userID = helper.GetUserDetails(header);
+
+            if(userID > -1) {
+
+                if(taskId != "") {
+
+                    Task task = taskRepository.findOne( taskId );
+
+                    System.out.println("My path" + filePath);
+                    if(task.getUserId() == userID) {
+
+                        FileAttachment file = new FileAttachment();
+                        file.setPath( filePath );
+                        file.setTaskId( taskId );
+                        file.setUserId( userID );
+                        fileAttachmentRepository.save( file );
+
+                        jsonObject.addProperty( "message", "File has been uploaded successfully for the User task." );
+                        jsonObject.addProperty( "taskId", file.getTaskId());
+                        jsonObject.addProperty( "fileId", file.getId());
+                        jsonObject.addProperty( "filePath", file.getPath() );
+                        return jsonObject.toString();
+                    }
+                    else {
+                        response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+                        jsonObject.addProperty( "message", "You are not allowed to perform this activity." );
+                        return jsonObject.toString();
+                    }
+                }
+                jsonObject.addProperty("message", "Invalid Task Id.");
+                return jsonObject.toString();
+            }
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            jsonObject.addProperty("message", "You are not authorized to perform this activity");
+            return jsonObject.toString();
+        }
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        jsonObject.addProperty("message", "You are not authorized to perform this activity");
         return jsonObject.toString();
+
     }
 
+    @RequestMapping(value="/tasks/{id}/attachments/{idAttachments}",method= RequestMethod.DELETE,produces="application/json")
+    public @ResponseBody String deleteFile (HttpServletRequest request, HttpServletResponse response) {
 
-    public String[] decode(String header){
-        assert header.substring(0, 6).equals("Basic");
-        String basicAuthEncoded = header.substring(6);
-        String basicAuthAsString = new String(Base64.getDecoder().decode(basicAuthEncoded.getBytes()));
-        final String[] credentialValues = basicAuthAsString.split(":", 2);
-        return  credentialValues;
+        JsonObject jsonObject = new JsonObject();
+
+        //Write delete file code here
+
+        String header = request.getHeader("Authorization");
+        String taskId = request.getRequestURI().split("/")[2];
+        String fileAttachmentId = request.getRequestURI().split( "/" )[4] ;
+        if(header != null) {
+
+            int userID = helper.GetUserDetails(header);
+
+            if(userID > -1) {
+
+                if(taskId != "") {
+                    if(fileAttachmentId !="") {
+
+                       FileAttachment file = fileAttachmentRepository.findOne(fileAttachmentId ) ;
+
+                       if(file != null) {
+                           Task task = taskRepository.findOne( taskId );
+                           if(task.getUserId() == userID){
+                           if (file.getTaskId().equals(taskId)) {
+                               if (file.getUserId() == userID) {
+                                   FileAttachment delFile = fileAttachmentRepository.findOne(file.getId());
+                                   fileAttachmentRepository.delete( delFile );
+                                   jsonObject.addProperty( "message", "File has been deleted successfully for the User task." );
+                                   return jsonObject.toString();
+                               } else {
+                                   response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+                                   jsonObject.addProperty( "message", "You are not allowed to perform this activity." );
+                                   return jsonObject.toString();
+                               }
+                           }
+                       }
+                           else {
+                               response.setStatus( HttpServletResponse.SC_FORBIDDEN );
+                               jsonObject.addProperty( "message", "You are not allowed to perform this activity." );
+                               return jsonObject.toString();
+                           }
+                       }
+
+                    }
+                    jsonObject.addProperty("message", "Invalid file Id.");
+                    return jsonObject.toString();
+                }
+                jsonObject.addProperty("message", "Invalid Task Id.");
+                return jsonObject.toString();
+            }
+        }
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        jsonObject.addProperty("message", "You are not authorized to perform this activity");
+        return jsonObject.toString();
+
     }
+
 }
