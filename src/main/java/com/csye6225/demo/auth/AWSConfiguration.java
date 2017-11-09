2ad80533a82febc47bbe4b12be984b5ce533f957
amitha murali <murali.a@husky.neu.edu>
2017-11-09 00:34:28 -0500

    assignment 8 updated

--- src/main/java/com/csye6225/demo/auth/AWSConfiguration.java
+++ src/main/java/com/csye6225/demo/auth/AWSConfiguration.java
@@ -2,7 +2,6 @@ package com.csye6225.demo.auth;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.ClasspathPropertiesFileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
@@ -10,6 +9,10 @@ import com.amazonaws.services.sns.AmazonSNSClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.regions.Regions;
 
 @Configuration
 public class AWSConfiguration {
@@ -37,11 +40,18 @@ public class AWSConfiguration {
     }
 
     @Bean
-    public AmazonSNSClient amazonSNSClient(AWSCredentials awsCredentials){
-
-        //create a new SNS client and set endpoint
+    public AmazonSNSClient amazonSNSClient(AWSCredentials awsCredentials) {
         AmazonSNSClient snsClient = new AmazonSNSClient(awsCredentials);
         snsClient.setRegion(Region.getRegion(Regions.fromName(region)));
         return snsClient;
+    }
+
+    @Bean
+    public AmazonDynamoDB amazonDynamoDBClient(AWSCredentials awsCredentials){
+
+        // This client will default to US West (Oregon)
+        AmazonDynamoDB dynamoDBclient = new AmazonDynamoDBClient(awsCredentials);
+        //dynamoDBclient.withRegion(Regions.fromName(region)).build();
+        return dynamoDBclient;
     }
 }
