f1fd73ee13a77c03b1d8697139035920b7897482
Jyoti Sharma <sharma.j@husky.neu.edu>
2017-11-08 19:50:42 -0500

    initialing assgn8

--- restartTomcat.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo systemctl restart tomcat8
--- src/main/java/com/csye6225/demo/service/S3Services.java
+++
@@ -1,7 +0,0 @@
-package com.csye6225.demo.service;
-
-import java.io.File;
-
-public interface S3Services {
-    public void uploadFile(String keyName, File file);
-}
